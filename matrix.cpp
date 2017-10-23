cee0b3635d127d6815bf48f670d8d2910b22ecde
Hekal <ahmhekal@gmail.com>
2017-10-23 21:17:13 +0200

    initiating project

---
+++ matrix.cpp
@@ -0,0 +1,1 @@
+//implemetation of class
---
+++ matrix.h
@@ -0,0 +1,13 @@
+//defenation of class
+class matrix{
+vector of vectors
+
+public:
+ matrix creatematrix function(string input)
+ {
+
+ }
+ matrix add (martrix a,matrix b)
+
+
+};
